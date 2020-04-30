9c74d1dd1de06788bc89db7ebad0f183625f6d3f
Franck <ftouanen@gmail.com>
2020-04-30 05:00:18 +0200

    files and source code comments cleaning

--- F1/Src/main.c
+++ F1/Src/main.c
@@ -1,33 +1,53 @@
 /*
-* STM32 HID Bootloader - USB HID bootloader for STM32F10X
-* Copyright (c) 2018 Bruno Freitas - bruno@brunofreitas.com
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Modified 20 April 2018
-*	by Vassilis Serasidis <info@serasidis.gr>
-*	This HID bootloader works with STM32F103 + STM32duino + Arduino IDE <http://www.stm32duino.com/>
-*
-* Modified January 2019
-*	by Michel Stempin <michel.stempin@wanadoo.fr>
-*	Cleanup and optimizations
+__ __| |           |  /_) |     ___|             |           |
+  |   __ \   _ \  ' /  | |  / |      _ \ __ \   |      _` | __ \   __|
+  |   | | |  __/  . \  |   <  |   |  __/ |   |  |     (   | |   |\__ \
+ _|  _| |_|\___| _|\_\_|_|\_\\____|\___|_|  _| _____|\__,_|_.__/ ____/
+  -----------------------------------------------------------------------------
+* TKG HID Bootloader - USB HID bootloader for STM32F10X family MCU
+  Copyright (C) 2019 by The KikGen labs.
+  LICENCE CREATIVE COMMONS - Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
 
-* Modified April 2020
-*	by TheKikgen Labs
-*	Runtime support of HD devices, various cleanup and optimizations, new state machine
-*
+  This file is part of the TKG-HID Bootloader distribution
+  https://github.com/TheKikGen/stm32-tkg-hid-bootloader
+  Copyright (c) 2020 TheKikGen Labs team.
+
+  Initial inspiration and parts of this project comes from the STM32 HID Bootloader,
+  from Bruno Freitas and Vassilis Serasidis <avrsite@yahoo.gr>,
+	Modified January 2019, 	by Michel Stempin <michel.stempin@wanadoo.fr>
+
+  -----------------------------------------------------------------------------
+  Disclaimer.
+
+  This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+  or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+  NON COMMERCIAL - PERSONAL USE ONLY : You may not use the material for pure
+  commercial closed code solution without the licensor permission.
+
+  You are free to copy and redistribute the material in any medium or format,
+  adapt, transform, and build upon the material.
+
+  You must give appropriate credit, a link to the github site
+  https://github.com/TheKikGen/stm32-tkg-hid-bootloader , provide a link to the license,
+  and indicate if changes were made. You may do so in any reasonable manner,
+  but not in any way that suggests the licensor endorses you or your use.
+
+  You may not apply legal terms or technological measures that legally restrict
+  others from doing anything the license permits.
+
+  You do not have to comply with the license for elements of the material
+  in the public domain or where your use is permitted by an applicable exception
+  or limitation.
+
+  No warranties are given. The license may not give you all of the permissions
+  necessary for your intended use.  This program is distributed in the hope that
+  it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 */
+
 
 #include <stm32f10x.h>
 #include <stdbool.h>
