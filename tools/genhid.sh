42f4f31defc9a73785f7ca494824b743967aca1a
TheKikGen <TheKikGen@gmail.com>
2021-01-20 17:01:32 +0100

    Small scipt to generate .h binary array for the upgrading bootloader from the Arduino IDE

---
+++ tools/genhid.sh
@@ -0,0 +1,4 @@
+rm ./tkg_hid_btl_uploader/tkg_hid_*.h
+./gencarray ../F1/bootloader_only_binaries/tkg_hid_generic_pc13.bin>./tkg_hid_btl_uploader/tkg_hid_generic_pc13.bin.h
+./gencarray ../F1/bootloader_only_binaries/tkg_hid_miditech4x4.bin>./tkg_hid_btl_uploader/tkg_hid_miditech4x4.bin.h
+./gencarray ../F1/bootloader_only_binaries/tkg_hid_midiplus_smartpad.bin>./tkg_hid_btl_uploader/tkg_hid_midiplus_smartpad.bin.h
--- tools/genhid_2.bat
+++ tools/genhid_2.bat
@@ -1,3 +1,4 @@
 del ./hid_btl_uploader/hid_*.h
-gencarray ../F1/bootloader_only_binaries/tkg_hid_generic_pc13.bin -ctkg_hid_generic_pc13 > ./tkg_hid_btl_uploader/tkg_hid_generic_pc13.bin.h
-gencarray ../F1/bootloader_only_binaries/tkg_hid_miditech4x4.bin  -ctkg_hid_miditech4x4 > ./tkg_hid_btl_uploader/tkg_hid_miditech4x4.bin.h
+gencarray ../F1/bootloader_only_binaries/tkg_hid_generic_pc13.bin  > ./tkg_hid_btl_uploader/tkg_hid_generic_pc13.bin.h
+gencarray ../F1/bootloader_only_binaries/tkg_hid_miditech4x4.bin   > ./tkg_hid_btl_uploader/tkg_hid_miditech4x4.bin.h
+gencarray ../F1/bootloader_only_binaries/tkg_hid_midiplus_smartpad.bin   > ./tkg_hid_btl_uploader/tkg_hid_midiplus_smartpad.bin.h
