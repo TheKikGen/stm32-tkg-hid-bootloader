40825adfb0dce6e77b580e740c3e24e4b0d810ad
TheKikGen <TheKikGen@gmail.com>
2021-01-20 16:57:42 +0100

    Makefile(s) for linux, mac, windows platform. nb : not tested on mac.

--- cli/make_win.sh
+++
@@ -1,7 +0,0 @@
-# To cross compile for windows, install mingw32
-# 
-i686-w64-mingw32-gcc -c -Wall -I . main.c -o main.o
-i686-w64-mingw32-gcc -c -Wall -I . hid-win.c -o hid-win.o
-i686-w64-mingw32-gcc -c -Wall -I . rs232.c -o rs232.o
-i686-w64-mingw32-gcc main.o hid-win.o rs232.o -lsetupapi -lhid -o tkg-flash.exe
-
